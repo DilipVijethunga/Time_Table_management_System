cc723b195511642220de3b48f4dfe70ce6b97545
dilip vijethunga <dilip.vijethunga@gmail.com>
2021-03-24 22:23:05 +0530

    first commit

---
+++ src/time_table_management_system/Time_Table_management_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package time_table_management_system;
+
+/**
+ *
+ * @author DILIP
+ */
+public class Time_Table_management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
