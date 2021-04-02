6565ed1677a0df34c7a8bbd5ddf68227566c8fc4
dilip vijethunga <dilip.vijethunga@gmail.com>
2021-04-02 14:15:04 +0530

    DB class

---
+++ src/time_table_management_system/DBConnect.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package time_table_management_system;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author DILIP
+ */
+public class DBConnect {
+    
+    
+    
+    public static Connection connect(){
+        Connection con = null;
+        
+        try {
+            Class.forName("con.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/timetable_mgmt","root","1234");
+        } 
+        catch (Exception e) {
+               System.out.println(e);
+        }
+        
+        return con;
+    
+    }
+    
+}
